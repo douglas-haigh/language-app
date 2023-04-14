f77764e3cfb80d257dbe5510b237b040fe8c651b
Douglas Haigh <douglashaigh16@thehutgroup.com>
2023-04-14 14:40:56 +0100

    First commit

---
+++ src/main/java/com/douglas/langaugeappbackend/LangaugeAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.douglas.langaugeappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangaugeAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangaugeAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/douglas/langaugeappbackend/LangaugeAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglas.langaugeappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangaugeAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
